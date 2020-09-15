b00986c664d7f60e9240208a326ab3def5d06a2f
Dmytro Tsyhanenko <sptrm@yahoo.com>
2020-09-15 16:33:43 +0200

    Initial commit

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ let app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+app.use(express.static("./public")); //damit schliessen wir die css an, weil man sonst kein Zugriff hat
+
+let entries = [
+    {title: "Ueberschrift", content: "Ich bin der Inhalt"}
+];
+
 app.get("/index", (req, res) => {
     res.render("index", {
         title: "Mars"
     });
 });
 
-app.listen(5000, () => {
+app.listen(5000, () => { //damit sagen wir das der Browser auf Port 5000 zuhoeren soll
     console.log("App wurde gestertet auf localhost:5000");
 });
