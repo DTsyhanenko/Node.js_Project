7008612ce3a4ee743f78da18fbebe11c932ee54c
Dmytro Tsyhanenko <sptrm@yahoo.com>
2020-09-16 21:27:53 +0200

    Initial commit

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 
 let express = require("express");
 // import express, { static } from "express"; eine neuere Version von dieser Schreibweise: Import
+let bodyParser = require("body-parser");
 let GuestbookEntry = require("./src/GuestbookEntry");
 
 let app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+app.use(bodyParser.urlencoded({extended: true})); //urlencoded setzen damit unser Formular richtig entgegengenommen
 app.use(express.static("./public")); //damit schliessen wir die css an, weil man sonst kein Zugriff hat
 
 let entries = [
@@ -15,10 +17,22 @@ let entries = [
     new GuestbookEntry("2. Eintrag", "Ich bin der Inhalt!"),
 ];
 
-app.get("/index", (req, res) => {
+app.get("/index", (req, res) => { //GET Anfrage auf der Seite
     res.render("index", {
         entries: entries
     });
+});
+
+app.post("/guestbook/new", (req, res) => { //POST Anfrage auf der Seite (wenn wir etwas aendern wollen)
+    let content = req.body.content;
+    let title = req.body.title;
+
+    let newEntry = new GuestbookEntry(title, content);
+    entries.push(newEntry);
+    //res.write("Erfolgreich!");
+    //res.end(); fuer eine Erfolgsmeldung ueber einen gespeicherten Beitrag
+
+    res.redirect("/index");
 });
 
 app.listen(5000, () => { //damit sagen wir das der Browser auf Port 5000 zuhoeren soll
