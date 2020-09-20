0513a93a3f98a0f8f8b7dcaabe6bcff820ae14c7
Dmytro Tsyhanenko <sptrm@yahoo.com>
2020-09-20 15:11:28 +0200

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,19 @@ module.exports = {
     output: {
         path: __dirname + "/public/build",
         filename: "bundle.js"
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
 }
